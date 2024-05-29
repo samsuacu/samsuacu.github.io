fde259cffa267f9de6d040f8848397adf68f3c82
samsuacu <35878540+samsuacu@users.noreply.github.com>
2024-05-30 02:57:00 +0700

    Update copy-rek.js

--- copy-rek.js
+++ copy-rek.js
@@ -1,114 +1,224 @@
-    
+/* Script Copy Nomor */
+/*
+function name() {
+  navigator.clipboard.writeText
+    ("Nomor");
+}
+*/
+function copyIrmawan() {
+  navigator.clipboard.writeText
+    ("8831720263");
+}
+function copyBayua() {
+  navigator.clipboard.writeText
+    ("8330320126");
+}
+function copyHendar() {
+  navigator.clipboard.writeText
+    ("1799559085");
+}
+function copyJayajp() {
+  navigator.clipboard.writeText
+    ("1760000248813");
+}
+function copyOmin() {
+  navigator.clipboard.writeText
+    ("088809052188");
+}
+function copyUutk() {
+  navigator.clipboard.writeText
+    ("081213632922");
+}
+function copyAsmara() {
+  navigator.clipboard.writeText
+    ("085892979633");
+}
+function copyCuplis() {
+  navigator.clipboard.writeText
+    ("0887433101841");
+}
+function copyAditi() {
+  navigator.clipboard.writeText
+    ("085811556369");
+}
+function copyGembel() {
+  navigator.clipboard.writeText
+    ("081901844540");
+}
+function copyUmilah() {
+  navigator.clipboard.writeText
+    ("085718533892");
+}
+function copyAria() {
+  navigator.clipboard.writeText
+    ("085814005871");
+}
 
-      /* Samsu */
-      function copySamsu() {
-        navigator.clipboard.writeText
-          ("082112590622");
-      }
-      function copyAllSamsu() {
-        navigator.clipboard.writeText
-          ("082112590622 an SAMSU");
-      }
+function copyNjay() {
+  navigator.clipboard.writeText
+    ("083899376707");
+}
+function copyAbdulr() {
+  navigator.clipboard.writeText
+    ("08989609301");
+}
+function copyMaldi() {
+  navigator.clipboard.writeText
+    ("083108269299");
+}
+function copyMedi() {
+  navigator.clipboard.writeText
+    ("083151495462");
+}
+function copyMaryam() {
+  navigator.clipboard.writeText
+    ("089630367095");
+}
+function cipyAbol() {
+  navigator.clipboard.writeText
+    ("083151716189");
+}
+function copyWhati() {
+  navigator.clipboard.writeText
+    ("081617938968");
+}
+function copyTohir() {
+  navigator.clipboard.writeText
+    ("08567913006");
+}
 
-      /* Mae */
-      function copyMae() {
-        navigator.clipboard.writeText
-          ("083807414835");
-      }
-      function copyAllMae() {
-        navigator.clipboard.writeText
-          ("083807414835 an MAE");
-      }
+function copyPeot() {
+  navigator.clipboard.writeText
+    ("081398601136 ");
+}
+function copyAnti() {
+  navigator.clipboard.writeText
+    ("085717206641");
+}
+function copyBancet() {
+  navigator.clipboard.writeText
+    ("088905989507");
+}
+function copySandi() {
+  navigator.clipboard.writeText
+    ("083879723368");
+}
+function copyDenih() {
+  navigator.clipboard.writeText
+    ("083897628029");
+}
+function copyDadan() {
+  navigator.clipboard.writeText
+    ("085781890237");
+}
+function copyJuned() {
+  navigator.clipboard.writeText
+    ("083811960626");
+}
+function copyYulita() {
+  navigator.clipboard.writeText
+    ("083160799017");
+}
+function copyAti() {
+  navigator.clipboard.writeText
+    ("085716749527");
+}
+function copyMilah() {
+  navigator.clipboard.writeText
+    ("085693069099");
+}
+function copyAri() {
+  navigator.clipboard.writeText
+    ("085694798957");
+}
+function copyOgi() {
+  navigator.clipboard.writeText
+    ("083153491889");
+}
+function copyEnos() {
+  navigator.clipboard.writeText
+    ("083874881715");
+}
+function copyGugun() {
+  navigator.clipboard.writeText
+    ("083876060649");
+}
+function copyRowi() {
+  navigator.clipboard.writeText
+    ("083871730484");
+}
+function copyEmen() {
+  navigator.clipboard.writeText
+    ("083813792806");
+}
+/* Ahong */
+function copyAhong() {
+  navigator.clipboard.writeText
+    ("082178365202");
+}
+/* Mega */
+function copyMega() {
+  navigator.clipboard.writeText
+    ("083159377466");
+}
+/* Robi */
+function copyCucu() {
+  navigator.clipboard.writeText
+    ("083811581787");
+}
+/* Nuryudi */
+function copyCulay() {
+  navigator.clipboard.writeText
+    ("083870895002");
+}
+/* Anwar */
+function copyBoan() {
+  navigator.clipboard.writeText
+    ("083811960627");
+}
+/* Abdulloh */
+function copyObuel() {
+  navigator.clipboard.writeText
+    ("083891604941");
+}
+/* Sri */
+function copySri() {
+  navigator.clipboard.writeText
+    ("083871790962");
+}
+/* Hudori */
+function copyHudori() {
+  navigator.clipboard.writeText
+    ("08990544234");
+}
+/* Pepew */
+function copyPepew() {
+  navigator.clipboard.writeText
+    ("083894115816");
+}
+/* Muhlis */
+function copyMuhlis() {
+  navigator.clipboard.writeText
+    ("083895944234");
+}
+/* Afiah */
+function copyAfiah() {
+  navigator.clipboard.writeText
+    ("088906244234");
+}
+/* Wulan */
+function copyWulan() {
+  navigator.clipboard.writeText
+    ("08815787468");
+}
+/* Mae */
+function copyMae() {
+  navigator.clipboard.writeText
+    ("083807414835");
+}
+/* Samsu */
+function copySamsu() {
+  navigator.clipboard.writeText
+    ("082112590622");
+}
 
-      /* Wulan */
-      function copyWulan() {
-        navigator.clipboard.writeText
-          ("08815787468");
-      }
-      function copyAllWulan() {
-        navigator.clipboard.writeText
-          ("08815787468 an WULAN SARI");
-      }
-
-      /* Afiah */
-      function copyAfiah() {
-        navigator.clipboard.writeText
-          ("088906244234");
-      }
-      function copyAllAfiah() {
-        navigator.clipboard.writeText
-          ("088906244234 an AFIAH");
-      }
-      
-      /* Muhlis */
-      function copyMuhlis() {
-        navigator.clipboard.writeText
-          ("083895944234");
-      }
-      function copyAllMuhlis() {
-        navigator.clipboard.writeText
-          ("083895944234 an MUHLIS");
-      }
-      
-      /* Pepew */
-      function copyPepew() {
-        navigator.clipboard.writeText
-          ("083894115816");
-      }
-      
-      /* Hudori */
-      function copyHudori() {
-        navigator.clipboard.writeText
-          ("08990544234");
-      }
-      function copyAllHudori() {
-        navigator.clipboard.writeText
-          ("08990544234 an AHMAD HUDORI");
-      }
-      
-      /* Sri */
-      function copySri() {
-        navigator.clipboard.writeText
-          ("083871790962");
-      }
-      
-      /* Abdulloh */
-      function copyObuel() {
-        navigator.clipboard.writeText
-          ("083891604941");
-      }
-      function copyAllObuel() {
-        navigator.clipboard.writeText
-          ("083891604941 an ABDULLOH");
-      }
-      
-      /* Anwar */
-      function copyBoan() {
-        navigator.clipboard.writeText
-          ("083811960627");
-      }
-      function copyAllBoan() {
-        navigator.clipboard.writeText
-          ("083811960627 an ANWAR");
-      }
-      
-      /* Nuryudi */
-      function copyCulay() {
-        navigator.clipboard.writeText
-          ("083870895002");
-      }
-      function copyAllCulay() {
-        navigator.clipboard.writeText
-          ("083870895002 an NNURYUDI");
-      }
-      
-      /* Robi */
-      function copyCucu() {
-        navigator.clipboard.writeText
-          ("083811581787");
-      }
-      function copyAllCucu() {
-        navigator.clipboard.writeText
-          ("083811581787 an ROBI GUNAWAN");
-      }
-      
