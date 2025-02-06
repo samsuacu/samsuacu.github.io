8025f897f3c400924f32ce354fd44894041723b5
samsuacu <35878540+samsuacu@users.noreply.github.com>
2025-02-06 23:52:51 +0700

    Add files via upload

---
+++ assets/js/copy-rek-jago1.js
@@ -0,0 +1,164 @@
+function copy() {
+    navigator.clipboard.writeText
+      ("");
+}
+function copy() {
+    navigator.clipboard.writeText
+      ("");
+}
+function copy() {
+    navigator.clipboard.writeText
+      ("");
+}
+function copy() {
+    navigator.clipboard.writeText
+      ("");
+}
+function copy() {
+    navigator.clipboard.writeText
+      ("");
+}
+function copy() {
+    navigator.clipboard.writeText
+      ("");
+}
+function copy() {
+    navigator.clipboard.writeText
+      ("");
+}
+function copy() {
+    navigator.clipboard.writeText
+      ("");
+}
+/* 32 */
+function copyAntonio() {
+    navigator.clipboard.writeText
+      ("102390248580");
+}
+function copyVincent() {
+    navigator.clipboard.writeText
+      ("104874817149");
+}
+function copyJose() {
+    navigator.clipboard.writeText
+      ("104844136881");
+}
+function copyBrian() {
+    navigator.clipboard.writeText
+      ("102115608651");
+}
+function copyVictor() {
+    navigator.clipboard.writeText
+      ("108844190325");
+}
+function copyBrandon() {
+    navigator.clipboard.writeText
+      ("101209331795");
+}
+function copyDaniel() {
+    navigator.clipboard.writeText
+      ("101105290785");
+}
+function copyMichael() {
+    navigator.clipboard.writeText
+      ("105925117907");
+}
+/* 24 */
+function copyJimmy() {
+    navigator.clipboard.writeText
+      ("104131569082");
+}
+function copyMarvin() {
+    navigator.clipboard.writeText
+      ("105828485962");
+}
+function copyTimothy() {
+    navigator.clipboard.writeText
+      ("108056638763");
+}
+function copyEdward() {
+    navigator.clipboard.writeText
+      ("105482567409");
+}
+function copyBobby() {
+    navigator.clipboard.writeText
+      ("101720983475");
+}
+function copyBruce() {
+    navigator.clipboard.writeText
+      ("101083964943");
+}
+function copyChris() {
+    navigator.clipboard.writeText
+      ("101331007870");
+}
+function copyRobert() {
+    navigator.clipboard.writeText
+      ("106262683918");
+}
+/* 16 */
+function copyEarl() {
+    navigator.clipboard.writeText
+      ("101522562269");
+}
+function copyAllen() {
+    navigator.clipboard.writeText
+      ("107498657144");
+}
+function copyGary() {
+    navigator.clipboard.writeText
+      ("103314105908");
+}
+function copySteven() {
+    navigator.clipboard.writeText
+      ("104686352664");
+}
+function copyRussell() {
+    navigator.clipboard.writeText
+      ("105151150513");
+}
+function copyBenjamin() {
+    navigator.clipboard.writeText
+      ("107488425580");
+}
+function copyThomas() {
+    navigator.clipboard.writeText
+      ("101426670126");
+}
+function copySusam() {
+    navigator.clipboard.writeText
+      ("104395226512");
+}
+/* 8 */
+function copyJohnny() {
+    navigator.clipboard.writeText
+      ("100381620401");
+}
+function copyNorman() {
+    navigator.clipboard.writeText
+      ("108351552616");
+}
+function copyMatthew() {
+    navigator.clipboard.writeText
+      ("104203661385");
+}
+function copyKenneth() {
+    navigator.clipboard.writeText
+      ("108547851501");
+}
+function copyCarlos() {
+    navigator.clipboard.writeText
+      ("107965923187");
+}
+function copyRoyy() {
+    navigator.clipboard.writeText
+      ("104548774237");
+}
+function copyJoseph() {
+    navigator.clipboard.writeText
+      ("101143736831");
+}
+function copyJames() {
+    navigator.clipboard.writeText
+      ("107329634913");
+}
